a2e80c79540f9d6ae2fd86a6bf40db198f40f965
Kevin Curbelo <kevin.curbelo@gmail.com>
2018-01-18 09:18:33 -0800

    Added more js to make the on click work

--- leader.js
+++ leader.js
@@ -11,13 +11,65 @@ class Application extends React.Component {
     }).then((jsonResult) => {
       // Do something with the result
       console.log(jsonResult);
-      let usd = jsonResult.USD
-      usd = JSON.stringify(usd);
-      let usdText = document.getElementById('usd-cont');
-      usdText.innerHTML = usd;
+      // selects the currency
+      let selectUSD = jsonResult.USD;
+      let selectEUR = jsonResult.EUR;
+      let selectSEK = jsonResult.SEK;
+      let selectGBP = jsonResult.GBP;
+      let selectAUD = jsonResult.AUD;
+      let selectJPY = jsonResult.JPY;
+      let selectCNY = jsonResult.CNY;
+
+      // Selects the price
+      let usdPrice = selectUSD.last
+      let eurPrice = selectEUR.last
+      let sekPrice = selectSEK.last
+      let gbpPrice = selectGBP.last
+      let audPrice = selectAUD.last
+      let jpyPrice = selectJPY.last
+      let cnyPrice = selectCNY.last
+
+
+
+      // Stringify json to get it to be able to be put on screen
+      usdPrice = JSON.stringify(usdPrice);
+      eurPrice = JSON.stringify(eurPrice);
+      sekPrice = JSON.stringify(sekPrice);
+      gbpPrice = JSON.stringify(gbpPrice);
+      audPrice = JSON.stringify(audPrice);
+      jpyPrice = JSON.stringify(jpyPrice);
+      cnyPrice = JSON.stringify(cnyPrice);      
+
+
+
+      // Set variable to element
+      // let usdText = document.getElementById('usd-cont');
+
+      // Change inner html
+      // usdText.innerHTML = usd;
+      
+      // Run function to change innerhtml
+      document.getElementById("usd-btn").addEventListener("click", usdFunction);
+      document.getElementById("eur-btn").addEventListener("click", usdFunction);
+      document.getElementById("sek-btn").addEventListener("click", usdFunction);
+      document.getElementById("gbp-btn").addEventListener("click", usdFunction);
+      document.getElementById("aud-btn").addEventListener("click", usdFunction);
+      document.getElementById("jpy-btn").addEventListener("click", usdFunction);
+      document.getElementById("cny-btn").addEventListener("click", usdFunction);
+
+      function usdFunction() {
+          document.getElementById("cur-main").innerHTML = usdPrice;
+      } 
+      function usdFunction() {
+          document.getElementById("cur-main").innerHTML = usdPrice;
+      }            
+
     })
   }
-  
+
+
+
+
   render() {
     return <div>
       <p >
@@ -26,6 +78,9 @@ class Application extends React.Component {
     </div>;
   }
 }
+
+
+
 
   ReactDOM.render(
     <Application />,
