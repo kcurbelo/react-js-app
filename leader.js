10ef9fed5f47368bd036bc95133c6fce94fe0a3c
Kevin Curbelo <kevin.curbelo@gmail.com>
2018-01-18 09:23:58 -0800

    Added all the js to make correct currencies work

--- leader.js
+++ leader.js
@@ -18,6 +18,7 @@ class Application extends React.Component {
       let selectGBP = jsonResult.GBP;
       let selectAUD = jsonResult.AUD;
       let selectJPY = jsonResult.JPY;
+      console.log(selectJPY)
       let selectCNY = jsonResult.CNY;
 
       // Selects the price
@@ -27,6 +28,7 @@ class Application extends React.Component {
       let gbpPrice = selectGBP.last
       let audPrice = selectAUD.last
       let jpyPrice = selectJPY.last
+      console.log(jpyPrice)
       let cnyPrice = selectCNY.last
 
 
@@ -38,6 +40,7 @@ class Application extends React.Component {
       gbpPrice = JSON.stringify(gbpPrice);
       audPrice = JSON.stringify(audPrice);
       jpyPrice = JSON.stringify(jpyPrice);
+      console.log(jpyPrice)
       cnyPrice = JSON.stringify(cnyPrice);      
 
 
@@ -50,19 +53,34 @@ class Application extends React.Component {
       
       // Run function to change innerhtml
       document.getElementById("usd-btn").addEventListener("click", usdFunction);
-      document.getElementById("eur-btn").addEventListener("click", usdFunction);
-      document.getElementById("sek-btn").addEventListener("click", usdFunction);
-      document.getElementById("gbp-btn").addEventListener("click", usdFunction);
-      document.getElementById("aud-btn").addEventListener("click", usdFunction);
-      document.getElementById("jpy-btn").addEventListener("click", usdFunction);
-      document.getElementById("cny-btn").addEventListener("click", usdFunction);
+      document.getElementById("eur-btn").addEventListener("click", eurFunction);
+      document.getElementById("sek-btn").addEventListener("click", sekFunction);
+      document.getElementById("gbp-btn").addEventListener("click", gbpFunction);
+      document.getElementById("aud-btn").addEventListener("click", audFunction);
+      document.getElementById("jpy-btn").addEventListener("click", jpyFunction);
+      document.getElementById("cny-btn").addEventListener("click", cnyFunction);
 
       function usdFunction() {
           document.getElementById("cur-main").innerHTML = usdPrice;
       } 
-      function usdFunction() {
-          document.getElementById("cur-main").innerHTML = usdPrice;
-      }            
+      function eurFunction() {
+          document.getElementById("cur-main").innerHTML = eurPrice;
+      }
+      function sekFunction() {
+          document.getElementById("cur-main").innerHTML = sekPrice;
+      } 
+      function gbpFunction() {
+          document.getElementById("cur-main").innerHTML = gbpPrice;
+      } 
+      function audFunction() {
+          document.getElementById("cur-main").innerHTML = audPrice;
+      } 
+      function jpyFunction() {
+          document.getElementById("cur-main").innerHTML = jpyPrice;
+      }  
+      function cnyFunction() {
+          document.getElementById("cur-main").innerHTML = cnyPrice;
+      }                               
 
     })
   }
